67677ea83dd03bee5a3a76db919868abfc0a3226
Chelsea Fisher <chelseasfisher1@gmail.com>
2022-01-10 18:44:54 -0600

    commit tree 3218d134c9388114afd74a0834df0cdd7077d0c3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers,drive) {
+    return drivers.filter (
+        (Match) => Match.toLowerCase() === drive.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, abc) {
+    return drivers.filter(
+        (match) => match.toLowerCase().indexOf(abc.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, names){
+    return drivers.filter((record) => record.name === names);
+}
